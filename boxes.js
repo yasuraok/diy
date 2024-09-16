c02e6de84380bf1dea49455f71447dae3b21ed00
Naoki Yasuraoka <yasuraok@gmail.com>
2024-09-16 20:28:07 +0900

    Add cutting diagram making function using flexbox

---
+++ boxes.js
@@ -0,0 +1,63 @@
+const colors = [
+    "#9B69B6",
+    "#56A085",
+    "#D36450",
+    "#7980B9",
+]
+
+// 直方体のデータを定義
+export const boxes = [
+    [
+        { w: 576, h: 260, c: colors[0]}, // 底
+        { w: 576, h: 260, c: colors[0]},
+        { w: 576, h: 260, c: colors[0]},
+        { w: 576, h: 260, c: colors[0]},
+        { w: 576, h: 260, c: colors[0]},
+        { w: 576, h: 260, c: colors[0]},
+    ],
+
+    [
+        { w: 576, h: 50, c: colors[1]}, // 前
+        { w: 576, h: 50, c: colors[1]},
+        { w: 576, h: 50, c: colors[1]},
+        { w: 576, h: 50, c: colors[1]},
+        { w: 576, h: 50, c: colors[1]},
+        { w: 576, h: 50, c: colors[1]},
+    ],
+
+    [
+        { w: 552, h: 50, c: colors[2]}, // 後 576 - 24
+        { w: 552, h: 50, c: colors[2]}, // 後
+        { w: 552, h: 50, c: colors[2]}, // 後
+        { w: 552, h: 50, c: colors[2]}, // 後
+        { w: 552, h: 50, c: colors[2]}, // 後
+        { w: 552, h: 50, c: colors[2]}, // 後
+    ],
+
+    [
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+        { w: 260, h: 45, c: colors[3]}, // 側
+    ]
+
+    // { w: 100,  h: 1, d: 1,   x: 0, y: 0, z: 0 },
+    // { w: 1,    h: 2, d: 1,   x: 2, y: 1, z: -1 }
+]
+
